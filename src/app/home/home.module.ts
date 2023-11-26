b82a1724c0eb65ae697d824d9de7fbf63821a7c0
Jagdish Patil <patil.jagdish222@gmail.com>
2023-11-26 17:43:58 +0530

    home page added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+
 const routes: Routes = [
   {
     path: '',
