84dd0b92a01b9a7cf1f125c146e11c3f30135ef2
Jagdish Patil <patil.jagdish222@gmail.com>
2023-11-27 00:20:35 +0530

    tab hover css added for dark and light mode

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,7 +36,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
       defaultTabId: 'profile',
       activeClasses: 'dark:text-white text-black border-primary border-b-4',
       inactiveClasses:
-        'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+        'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-transparent dark:border-tranparent dark:hover:text-gray-300',
       onShow: () => {},
     };
     // instance options with default values
